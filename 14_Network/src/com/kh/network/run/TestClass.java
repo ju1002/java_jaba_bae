c703dcb54cbefc33cfefe5591381d0929f89bf98
ju1002 <hihill323105@gmail.com>
2025-08-21 12:02:41 +0900

    hi

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -3,6 +3,7 @@ package com.kh.network.run;
 public class TestClass {
 
 	public static void main(String[] args) {
+		
 //fasdfsadfsdfsd
 		System.out.println("까망이");
 		//아 고생했다 주영아 그제 맞제 그제앙
@@ -14,10 +15,14 @@ public class TestClass {
 	
 	public void hahah() {
 		System.out.println("아 심심하다잉~~~~");
+		System.out.println(1);
+		
+		//아래 순서로 remote에 올리고 가져오고 해서 작업을 해보자
 		System.out.println("1. 워크스페이스에서 git Bash를 실행");
 		System.out.println("2. git pull origin master");
 		System.out.println("3. git add .");
 		System.out.println("4. git commit -m 뭐했는지");
 		System.out.println("5. git push origin master");
 	}
+	
 }
