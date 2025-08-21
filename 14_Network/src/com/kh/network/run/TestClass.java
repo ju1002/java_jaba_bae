0914347bd7c31d9a2d43b84aaa04631dadf588be
ju1002 <hihill323105@gmail.com>
2025-08-21 11:21:57 +0900

    edd

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -4,7 +4,15 @@ public class TestClass {
 
 	public static void main(String[] args) {
 //fasdfsadfsdfsd
-		
+		System.out.println("까망이");
+		//아 고생했다 주영아 그제 맞제 그제앙
 	}
-
+	public void dkdk() {
+		System.out.println("rkfkt");
+	}
+	
+	
+	public void hahah() {
+		System.out.println("아 심심하다잉~~~~");
+	}
 }
